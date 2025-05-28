8b3b2c78d4f95c6a883520929d5fc70eefe8e174
M-L-Ml <m.l.malinouski@gmail.com>
2025-05-28 11:00:41 +0200

    delete AppInsights.WindowsDesktop

--- src/app/GitUI/GlobalUsings.cs
+++ src/app/GitUI/GlobalUsings.cs
@@ -1,2 +1,4 @@
-// This file globally aliases System.Windows.Forms to Modern.Forms for cross-platform migration.
+﻿// This file globally aliases System.Windows.Forms to Modern.Forms for cross-platform migration.
 // Add more aliases as needed for other namespaces/classes.
+global using System.Windows.Forms;
+global using System.Drawing;
