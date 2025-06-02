5a7bdf1fa1d722cf5b7c3c9c3807bf880f45321d
M-L-Ml <m.l.malinouski@gmail.com>
2025-06-02 15:36:18 +0200

    - stub copy TaskDialogPage

--- src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
+++ src/app/GitUI/UserControls/RevisionGrid/RevisionGridControl.cs
@@ -3326,18 +3326,3 @@ namespace GitUI
         }
     }
 }
-
-#if !WINDOWS_OWN
-namespace System.Windows.Forms
-{
-    public class TaskDialogPage
-    {
-        public string Text { get; set; }
-        public string Caption { get; set; }
-        public string Heading { get; set; }
-        public object Icon { get; set; }
-        public object Verification { get; set; }
-        public bool SizeToContent { get; set; }
-    }
-}
-#endif
