07275b60d595c0ec97d046e33e04a2441fd74425
M-L-Ml <m.l.malinouski@gmail.com>
2025-06-04 21:01:53 +0200

    Fix merge

--- src/app/GitExtUtils/GitUI/Theming/ThemeFix.cs
+++ src/app/GitExtUtils/GitUI/Theming/ThemeFix.cs
@@ -80,14 +80,14 @@ namespace GitExtUtils.GitUI.Theming
 
         private static void SetupLinkLabel(this LinkLabel label)
         {
-            label.LinkColor = Application.IsDarkModeEnabled ? Color.CornflowerBlue : label.LinkColor.AdaptTextColor();
+            label.LinkColor = Application2.IsDarkModeEnabled ? Color.CornflowerBlue : label.LinkColor.AdaptTextColor();
             label.VisitedLinkColor = label.VisitedLinkColor.AdaptTextColor();
             label.ActiveLinkColor = label.ActiveLinkColor.AdaptTextColor();
         }
 
         private static void SetupToolStripStatusLabel(this ToolStripLabel label)
         {
-            label.LinkColor = Application.IsDarkModeEnabled ? Color.CornflowerBlue : label.LinkColor.AdaptTextColor();
+            label.LinkColor = Application2.IsDarkModeEnabled ? Color.CornflowerBlue : label.LinkColor.AdaptTextColor();
             label.VisitedLinkColor = label.VisitedLinkColor.AdaptTextColor();
             label.ActiveLinkColor = label.ActiveLinkColor.AdaptTextColor();
         }
