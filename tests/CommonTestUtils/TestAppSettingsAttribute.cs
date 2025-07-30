b6a4fb65932d6c5d928e352b855b934cb7653355
M-L-Ml <m.l.malinouski@gmail.com>
2025-07-30 14:29:54 +0200

    Use INamedSemaphore for Linux  - from BuildXL

--- tests/CommonTestUtils/TestAppSettingsAttribute.cs
+++ tests/CommonTestUtils/TestAppSettingsAttribute.cs
@@ -1,13 +1,19 @@
 ﻿using GitCommands;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
-
+using BuildXL.Utilities.Core;
+using GitExtUtils;
 namespace CommonTestUtils
 {
     [AttributeUsage(AttributeTargets.Assembly)]
     public sealed class TestAppSettingsAttribute : Attribute, ITestAction
     {
-        private readonly Semaphore _semaphore = new(initialCount: 1, maximumCount: 1, "GitExtensionsTestAssemblySerializer");
+        private readonly INamedSemaphore _semaphore = GetSemaphore();
+
+        private static INamedSemaphore GetSemaphore()
+        {
+            return SemaphoreFactory.CreateOrOpen(initialCount: 1, maximumCount: 1, name: (EnvUtils.RunningOnWindows() ? "" : "/") + "GitExtensionsTestAssemblySerializer").ThrowIfFailure().Result;
+        }
 
         public ActionTargets Targets => ActionTargets.Suite;
 
