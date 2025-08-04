3e4c7471c914b447d2d16c55c81e997fa7444925
M-L-Ml <m.l.malinouski@gmail.com>
2025-08-04 13:55:53 +0200

    Fix , Improve build scripts 2

--- build/scripts/package.linux.sh
+++ build/scripts/package.linux.sh
@@ -94,7 +94,8 @@ mv -v $APPNAME.AppDir/usr/share/applications/{$APPNAMEkey,com.$APPNAMEkey_scm.$A
 
 # Copy icon
 cp resources/appimage/gitextensions.png $APPNAME.AppDir/com.$APPNAMEkey_scm.$APPNAME.png
-ln -v -rsf $APPNAME.AppDir/opt/$APPNAMEopt/$APPNAME $APPNAME.AppDir/AppRun
+ExeName=GitExtensions
+ln -v -rsf $APPNAME.AppDir/opt/$APPNAMEopt/$ExeName $APPNAME.AppDir/AppRun
 ln -rsf $APPNAME.AppDir/usr/share/applications/com.$APPNAMEkey_scm.$APPNAME.desktop $APPNAME.AppDir
 
 # Copy appdata
@@ -149,7 +150,7 @@ mkdir -p "$DEB_BUILD_DIR/DEBIAN"
 
 # Copy application files
 cp -fr $BUILDSRC/* "$DEB_BUILD_DIR/opt/$APPNAMEkey/"
-ln -rsf "$DEB_BUILD_DIR/opt/$APPNAMEkey/$APPNAMEkey" "$DEB_BUILD_DIR/usr/bin/"
+ln -rsf "$DEB_BUILD_DIR/opt/$APPNAMEkey/$ExeName" "$DEB_BUILD_DIR/usr/bin/"
 
 # Copy desktop files and icons
 cp -r resources/_common/applications "$DEB_BUILD_DIR/usr/share/"
